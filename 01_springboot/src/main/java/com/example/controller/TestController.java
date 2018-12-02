f6ad378d5137c3963d85ec1c09059bbf687bb977
13669005514 <13669005514@163.com>
2018-12-02 21:50:37 +0800

    HelloWord测试

---
+++ 01_springboot/src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description:
+ * 测试使用springBoot
+ * @author: fxzhang
+ * @Date: 2018/11/30 20:54
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Map<String, List<Integer>> sayHello() {
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            integers.add(i);
+        }
+        HashMap<String, List<Integer>> map = new HashMap<>(15);
+        map.put("hello",integers);
+        return map;
+    }
+
+
+
+}
